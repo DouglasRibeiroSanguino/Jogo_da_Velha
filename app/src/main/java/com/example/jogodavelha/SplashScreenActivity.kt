18caf5af075a5ccc61391ae72a0e5c1c7a0a5000
perkles <douglasribeirosanguino3@gmail.com>
2021-09-18 09:53:55 -0300

    Jogo da Velha em Kotlin

---
+++ app/src/main/java/com/example/jogodavelha/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.jogodavelha
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jogodavelha.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivFirst.alpha =0f
+        binding.ivFirst.animate().setDuration(1600).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
